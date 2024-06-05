6b3629e092072037c0c9270c7e4f90fc6ed07d7d
1140444693@qq.com <1140444693@qq.com>
2024-06-05 17:52:25 +0800

    优化分页组件

--- src/app/ui/pagination.tsx
+++ src/app/ui/pagination.tsx
@@ -20,10 +20,10 @@ export default function Pagination() {
       </div>
       <div className="flex items-center">
         <span className="mr-6 text-color-desc">当前1-10条，总计200条</span>
-        <nav className="isolate inline-flex -space-x-px" aria-label="Pagination">
+        <nav className="inline-flex" aria-label="Pagination">
           <a
             href="#"
-            className="relative inline-flex items-center  px-2 py-2 text-gray-400  hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            className="relative inline-flex items-center justify-center w-[30px] h-[30px] rounded-l-md mr-2 text-gray-400  hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
           >
             <span className="sr-only">Previous</span>
             <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
@@ -32,46 +32,24 @@ export default function Pagination() {
           <a
             href="#"
             aria-current="page"
-            className="relative z-10 inline-flex items-center rounded-lg border border-indigo-600 px-4 py-2 text-sm font-semibold text-indigo-600 focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="relative z-10 inline-flex items-center justify-center w-[30px] h-[30px] mr-2 rounded-lg border border-indigo-600 text-sm font-semibold text-indigo-600 focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             1
           </a>
+          {new Array(5).fill("").map((item, index) => {
+            return (
+              <a
+                key={index}
+                href="#"
+                className="relative inline-flex items-center justify-center w-[30px] h-[30px] rounded-lg mr-2 text-sm font-semibold text-gray-900  hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+              >
+                {index + 2}
+              </a>
+            );
+          })}
           <a
             href="#"
-            className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900  hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-          >
-            2
-          </a>
-          <a
-            href="#"
-            className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900  hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-          >
-            3
-          </a>
-          <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700  focus:outline-offset-0">
-            <EllipsisHorizontalIcon className="w-5 h-5" />
-          </span>
-          <a
-            href="#"
-            className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900  hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-          >
-            8
-          </a>
-          <a
-            href="#"
-            className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900  hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-          >
-            9
-          </a>
-          <a
-            href="#"
-            className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900  hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-          >
-            10
-          </a>
-          <a
-            href="#"
-            className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400  hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            className="relative inline-flex items-center justify-center w-[30px] h-[30px] rounded-r-md text-gray-400  hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
           >
             <span className="sr-only">Next</span>
             <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
