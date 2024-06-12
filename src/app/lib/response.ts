8f3070b98a2fffed1e2a37c9e8af9de7f5a46887
1140444693@qq.com <1140444693@qq.com>
2024-06-12 10:36:41 +0800

    新增框架通用工具库

---
+++ src/app/lib/response.ts
@@ -0,0 +1,41 @@
+const json = {
+    code: 1,
+    msg: "",
+    data: null
+  };
+  
+  function success(msg: string, data: any = null) {
+    json.code = 1;
+    json.msg = msg;
+    json.data = data;
+    return Response.json(json);
+  }
+  
+  function error(msg: string, data: any = null) {
+    json.code = 0;
+    json.msg = msg;
+    json.data = data;
+    return Response.json(json);
+  }
+  
+  function errorWithCode(code: number,msg: string, data: any = null) {
+    json.code = code;
+    json.msg = msg;
+    json.data = data;
+    return Response.json(json);
+  }
+  
+  export interface ApiResponse<T> {
+    status: number;
+    message: string;
+    data: T;
+  }
+  
+  const api = {
+    success,
+    error,
+    errorWithCode
+  };
+  
+  export default api
+  
