e986a00a32ea7a5be97eb3b59e9cd4c33999c017
1140444693@qq.com <1140444693@qq.com>
2024-06-12 17:56:35 +0800

    完善api模板文件

--- src/app/api/template/[id]/route.ts
+++ src/app/api/template/[id]/route.ts
@@ -1,16 +1,18 @@
+import { NextRequest } from "next/server";
 import api from "@/app/lib/response";
 
+
 // 获取用户
-export function GET(req: Request, { params }: { params: { id: string } }) {
+export function GET(request: NextRequest, { params }: { params: { id: string } }) {
   return api.success("获取成功！");
 }
 
 // 更新用户
-export function PUT(req: Request, { params }: { params: { id: string } }) {
+export function PUT(request: NextRequest, { params }: { params: { id: string } }) {
   return api.success("更新成功！");
 }
 
 // 删除用户
-export function DELETE(req: Request, { params }: { params: { id: string } }) {
+export function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
   return api.success("删除成功！");
 }
--- src/app/api/template/route.ts
+++ src/app/api/template/route.ts
@@ -1,11 +1,17 @@
+
+import { NextRequest } from "next/server";
 import api from "@/app/lib/response";
 
 // 获取用户列表
-export function GET() {
+export async function GET(request: NextRequest) {
   return api.success("获取成功！");
 }
 
+export async function save(request: NextRequest, id: string = "") {
+
+}
+
 // 创建用户
-export function POST() {
+export async function POST(request: NextRequest) {
   return api.success("创建成功！");
 }
