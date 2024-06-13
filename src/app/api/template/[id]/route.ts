78df31a7a2b1d5e6191862ede5549dd27f21d531
1140444693@qq.com <1140444693@qq.com>
2024-06-13 18:01:03 +0800

    优化示例模板文件

--- src/app/api/template/[id]/route.ts
+++ src/app/api/template/[id]/route.ts
@@ -3,16 +3,16 @@ import api from "@/app/lib/response";
 
 
 // 获取用户
-export function GET(request: NextRequest, { params }: { params: { id: string } }) {
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
   return api.success("获取成功！");
 }
 
 // 更新用户
-export function PUT(request: NextRequest, { params }: { params: { id: string } }) {
+export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
   return api.success("更新成功！");
 }
 
 // 删除用户
-export function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
   return api.success("删除成功！");
 }
