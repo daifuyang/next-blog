3ef7807e31463564c31b61058e37d7f7e4892e80
1140444693@qq.com <1140444693@qq.com>
2024-06-13 18:00:42 +0800

    排除用户密码字段

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -12,7 +12,20 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient().$extends({
+    result: {
+      cmsUser: {
+        password: {
+          needs: {},
+          compute() {
+            return undefined;
+          }
+        }
+      }
+    }
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
